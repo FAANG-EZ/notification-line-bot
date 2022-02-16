4c86732559d16321ded70af6dc990c60812a9555
Louis.Su <i0989872540@gmail.com>
2022-02-16 23:55:39 +0800

    test: github actions & ssl

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        # TextSendMessage(text=event.message.text)
-        "測試 Line 機器人是否正常運行"
+        TextSendMessage(text=event.message.text)
     )
 
 if __name__ == "__main__":
