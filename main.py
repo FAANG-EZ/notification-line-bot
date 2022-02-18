0c7f6d04f1a9869d0493b8e323a346dab1d95dce
Louis.Su <i0989872540@gmail.com>
2022-02-19 00:07:15 +0800

    chore: some stupid problem

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ app = Flask(__name__)
 
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 channel_secret = os.getenv('LINE_CHANNEL_SECRET')
-if channel_access_token:
+if not channel_access_token:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
-if channel_secret:
+if not channel_secret:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
 
