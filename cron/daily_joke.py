5cd3ce777d4b3c0220acb0c11e042e54351277ff
Louis.Su <i0989872540@gmail.com>
2022-03-13 02:50:27 +0800

    fix: cron run different time and messages on weekday or weekend

--- cron/daily_joke.py
+++ cron/daily_joke.py
@@ -22,10 +22,17 @@ def daily_joke(app):
     scheduler = APScheduler()
     scheduler.init_app(app)
 
-    @scheduler.task('cron', id='write_log', hour=9, minute=30)
-    def write_log():
+    @scheduler.task('cron', id='weekday_joke', day_of_week='mon-fri', hour=9, minute=30)
+    def weekday_joke():
         group_id = os.getenv('GROUP_ID')
         line_bot_api.push_message(group_id, TextSendMessage(text='各位社畜早安，早餐吃了嗎？還有多少 issues 沒解？今天會不會又噴什麼垃圾 bug？為你們送上今天的笑話，祝你們今天上班愉快。'))
+        joke = jokekappa.get_joke()
+        line_bot_api.push_message(group_id, TextSendMessage(text=joke['content']))
+
+    @scheduler.task('cron', id='weekend_joke', day_of_week='sat-sun', hour=11, minute=30)
+    def weekend_joke():
+        group_id = os.getenv('GROUP_ID')
+        line_bot_api.push_message(group_id, TextSendMessage(text='各位放假仔早安，為你們送上今天的笑話，祝你們今天放假愉快。'))
         joke = jokekappa.get_joke()
         line_bot_api.push_message(group_id, TextSendMessage(text=joke['content']))
 
