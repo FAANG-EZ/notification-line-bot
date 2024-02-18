5f8bdc3c70660eb6c8faf467f373237fd0a56c37
louis_su <louis_su@trendmicro.com>
2024-02-18 16:54:52 +0800

    refactor: joke & ithome module from function to class

--- api/controller/callback.py
+++ api/controller/callback.py
@@ -1,20 +1,23 @@
-import jokekappa
-
 import re
 
-from flask import Flask, request, abort
-
-from helper.linebot_init import line_bot
-
+from flask import Flask, abort, request
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
 )
+
+from helper.joke import Joke
+from helper.linebot_init import line_bot
+from logger.logger import logger
 
 app = Flask(__name__)
 
 line_bot = line_bot()
 line_bot_api = line_bot.line_bot_api
 handler = line_bot.handler
+
+joke = Joke()
 
 class callback_controller:
     def __init__(self):
@@ -29,16 +32,14 @@ class callback_controller:
         try:
             handler.handle(body, signature)
         except InvalidSignatureError:
-            print("Invalid signature. Please check your channel access token/channel secret.")
+            logger.error("Invalid signature. Please check your channel access token/channel secret.")
             abort(400)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
     if re.match('^說個笑話$',message):
-        joke = jokekappa.get_joke()
-
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=joke['content'])
+            TextSendMessage(text=joke.generate_joke())
         )
